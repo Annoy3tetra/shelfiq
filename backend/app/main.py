5a443cb9e546152c69a950e87c8fc892e81165ac
Annoy3tetra <harsh185546@gmail.com>
2026-07-16 16:19:34 +0530

    Configure Production API

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.parsed_cors_origins,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origin_regex=r"https://shelfiq-three.vercel.app/",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
